7c38cb3e606eaae1e52aca5f11b096e2641329b4
Sec-Nadine <1835477566@qq.com>
2020-12-21 17:57:13 +0800

    hackthebox

--- PythonScript/md5.py
+++ PythonScript/md5.py
@@ -1,24 +1,15 @@
 import requests
 import hashlib
 import re
+url="http://167.99.85.197:32033/"
 
-req = requests.session()
-url = "http://167.99.85.197:32033/"
-
-rget = req.get(url)
-html = rget.content
-
-def html_tags(html):
-    clean = re.compile('<.*?>')
-    return re.sub(clean, '',html)
-
-out1 = html_tags(html)
-out2 = out1.split('string')[1]
-out3 = out2.rstrip()
-
-mdHash = hashlib.md5(out3).hexdigest()
-
-data = dict(hash=mdHash)
-rpost = req.post(url=url,data=data)
-
-print(rpost.text)
+r=requests.session()
+out=r.get(url)
+out=re.search("<h3 align='center'>+.*?</h3>",out.text)
+out=re.search("'>.*<",out[0])
+out=re.search("[^|'|>|<]...................",out[0])
+out=hashlib.md5(out[0].encode('utf-8')).hexdigest()
+print("sending md5 :-{}".format(out))
+data={'hash': out}
+out = r.post(url = url, data = data)
+print(out.text)
